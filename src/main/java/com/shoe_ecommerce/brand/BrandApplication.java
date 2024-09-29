b48ff13b8d57d04318f13b7d9f0e2d34b16d84c9
JasonCrk <emerzonkolki@gmail.com>
2024-09-29 12:14:43 -0500

    feat: abstraction Service annotation

--- src/main/java/com/shoe_ecommerce/brand/BrandApplication.java
+++ src/main/java/com/shoe_ecommerce/brand/BrandApplication.java
@@ -1,9 +1,17 @@
 package com.shoe_ecommerce.brand;
 
+import com.shoe_ecommerce.brand.shared.domain.Service;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
+@ComponentScan(
+		includeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Service.class),
+		value = { "com.shoe_ecommerce.brand" }
+)
 public class BrandApplication {
 
 	public static void main(String[] args) {
