7fdb77c46445255536dbdb14597a9a6d4c35f1cb
JasonCrk <emerzonkolki@gmail.com>
2024-10-01 00:06:42 -0500

    feat: Delete brand endpoint

---
+++ src/main/java/com/shoe_ecommerce/brand/context/presentation/controllers/v1/BrandDeleteController.java
@@ -0,0 +1,30 @@
+package com.shoe_ecommerce.brand.context.presentation.controllers.v1;
+
+import com.shoe_ecommerce.brand.context.application.commands.delete.DeleteBrandCommand;
+import com.shoe_ecommerce.brand.shared.domain.bus.command.CommandBus;
+import com.shoe_ecommerce.brand.shared.domain.bus.query.QueryBus;
+import com.shoe_ecommerce.brand.shared.infrastructure.RestApiController;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/brands")
+public final class BrandDeleteController extends RestApiController {
+
+    private BrandDeleteController(CommandBus commandBus, QueryBus queryBus) {
+        super(commandBus, queryBus);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> index(@PathVariable("id") UUID id) {
+        this.dispatch(new DeleteBrandCommand(id.toString()));
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
