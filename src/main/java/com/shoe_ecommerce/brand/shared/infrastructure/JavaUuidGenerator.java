a2b2280e381d273502bd50eeed4d45ea47cd2465
JasonCrk <emerzonkolki@gmail.com>
2024-09-30 21:06:16 -0500

    fix(JavaUuidGenerator.java): Service annotation

--- src/main/java/com/shoe_ecommerce/brand/shared/infrastructure/JavaUuidGenerator.java
+++ src/main/java/com/shoe_ecommerce/brand/shared/infrastructure/JavaUuidGenerator.java
@@ -1,9 +1,11 @@
 package com.shoe_ecommerce.brand.shared.infrastructure;
 
+import com.shoe_ecommerce.brand.shared.domain.Service;
 import com.shoe_ecommerce.brand.shared.domain.UuidGenerator;
 
 import java.util.UUID;
 
+@Service
 public class JavaUuidGenerator implements UuidGenerator {
 
     @Override
