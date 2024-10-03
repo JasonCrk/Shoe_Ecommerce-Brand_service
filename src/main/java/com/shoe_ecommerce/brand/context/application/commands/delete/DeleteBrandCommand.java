8cdeaacdbb2304226dbdf4cd9746d5a970c8821f
JasonCrk <emerzonkolki@gmail.com>
2024-10-03 11:27:12 -0500

    feat(application/commands): Remove delete brand command

--- src/main/java/com/shoe_ecommerce/brand/context/application/commands/delete/DeleteBrandCommand.java
+++
@@ -1,6 +0,0 @@
-package com.shoe_ecommerce.brand.context.application.commands.delete;
-
-import com.shoe_ecommerce.brand.shared.domain.bus.command.Command;
-
-public record DeleteBrandCommand(String id) implements Command {
-}
