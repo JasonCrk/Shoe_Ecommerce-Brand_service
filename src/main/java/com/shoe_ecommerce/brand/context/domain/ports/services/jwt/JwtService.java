e55cf606c1db5e61356de503c81015c798490d9a
JasonCrk <emerzonkolki@gmail.com>
2024-10-03 09:52:11 -0500

    feat: Remove JWT service and auth middleware

--- src/main/java/com/shoe_ecommerce/brand/context/domain/ports/services/jwt/JwtService.java
+++
@@ -1,7 +0,0 @@
-package com.shoe_ecommerce.brand.context.domain.ports.services.jwt;
-
-import com.shoe_ecommerce.brand.context.shared.domain.AuthUser;
-
-public interface JwtService {
-    AuthUser getUserFromToken(String token);
-}
